25c87d3d8183f6d0461b4d60602243b06956e888
Fesaa <77553571+Fesaa@users.noreply.github.com>
2022-12-06 12:51:34 +0100

    Disconnect tracker to accurately track your party status

---
+++ src/main/java/fesa/needyfesa/mixin/MinecraftClientMixin.java
@@ -0,0 +1,18 @@
+package fesa.needyfesa.mixin;
+
+import fesa.needyfesa.cubeCode.CubeVarManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+    @Inject(at = @At(value = "INVOKE", target= "Lnet/minecraft/client/MinecraftClientGame;onLeaveGameSession()V"), method="disconnect(Lnet/minecraft/client/gui/screen/Screen;)V")
+    public void onDisconnect(Screen s, CallbackInfo info) {
+        CubeVarManager.partyStatus = false;
+    }
+}
